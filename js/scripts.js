427b088421cd4ff6b599f5621a18ff47189faf2a
John Linneman <urielpro78@gmail.com>
2018-03-23 16:23:38 -0700

    add js functionality to question 1

--- js/scripts.js
+++ js/scripts.js
@@ -8,32 +8,37 @@ $(document).ready(function() {
     var question4 = $("select#security").val();
     var question5 = $("select#mobile").val();
 
-    var collate = (question1 + question2 + question3 + question4 + question5)
-
-    if (question1 === yes) {
-
-    }
-
-    console.log(collate);
 
 
+    if (question1 === "yes" && question2 === "no" && question3 === "no" && question4 === "no" &&  question5 === "no") {
 
+    $("#design").show();
 
+  }  if ( question2 === "yes" && question1 === "no" && question3 === "no" && question4 === "no" && question5 === "no") {
 
+    $("PHP").show();
+  }
 
 
 
+//   } else if (question1 === "no" && question2 === "yes" && question3 === "no" && question4 === "no" && question5 === "no") {
+//
+//     $("#PHP").show();
+//
+//   } else if (question1 === "no" && question2 === "" && question3 === "yes" && question4 === "no" && question5 === "no") {
+//
+//     $("#ruby").show();
+//
+// }   else if (question1 === "no" && question2 === "no" && question3 === "no" && question4 === "yes" && question5 === "no") {
+//
+//     $("#c-sharp").show();
+//
+// }   else if (question1 === "no" && question2 === "no" && question3 === "no" && question4 === "no" && question5 === "yes") {
+//     $("#android").show();
+// }
+//
 
 
-
-
-
-
-
-
-
-
-
-  });
+});
 
 });
