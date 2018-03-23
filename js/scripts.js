31c87d0c260edfb7b89022678d485971235780ea
John Linneman <urielpro78@gmail.com>
2018-03-23 13:26:19 -0700

    add variables to capture values from select id's, add more questions for survey, add columns and rows to organize layout of page better

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
-  $("form#programming-questions").submit(function(event) {
-    event.preventDefault();
-    var input = parseInt$("#fonts").val();
-    console.log("working");
+  $("#coding-survey").submit(function(event) {
+
+    var question1 = parseInt("select#font-question1").val();
+    var question2 = parseInt("select#big-tech").val();
+    var question3 = parseInt("select#start-up")
+
+    console.log(question1);
+
+
+
+
+
+
+
+
+
+
+
+      event.preventDefault();
 
   });
 
