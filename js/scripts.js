f651902b09e1f8a10e70fe7f292b96d7c4e6edb1
John Linneman <urielpro78@gmail.com>
2018-03-23 15:11:12 -0700

    add javascript variables to gather values and branching to implement choices from survey, add bootstrap card classes to create answers for questions

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,20 @@ $(document).ready(function() {
     var question1 = $("select#font-question1").val();
     var question2 = $("select#big-tech").val();
     var question3 = $("select#start-up").val();
+    var question4 = $("select#security").val();
+    var question5 = $("select#mobile").val();
 
-    var collate = (question1, question2, question3)
+    var collate = (question1 + question2 + question3 + question4 + question5)
+
+    if (question1 === yes) {
+
+    }
+
     console.log(collate);
+
+
+
+
 
 
 
