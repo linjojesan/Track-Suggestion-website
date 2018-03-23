c9ca97ad68ea6104f8b4854d802cae787258af69
John Linneman <urielpro78@gmail.com>
2018-03-23 14:03:22 -0700

    fix js script problem and replace button class with input=submit class, test with console.log and received values from select boxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,27 @@
 $(document).ready(function() {
   $("#coding-survey").submit(function(event) {
-
-    var question1 = parseInt("select#font-question1").val();
-    var question2 = parseInt("select#big-tech").val();
-    var question3 = parseInt("select#start-up")
-
-    console.log(question1);
-
-
-
-
-
-
-
-
-
-
-
       event.preventDefault();
+
+    var question1 = $("select#font-question1").val();
+    var question2 = $("select#big-tech").val();
+    var question3 = $("select#start-up").val();
+
+    var collate = (question1, question2, question3)
+    console.log(collate);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   });
 
